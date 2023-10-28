f69fed09e45e41fa1842400e079fdd52ef13beea
Davi Antonaji <davi@antonaji.com.br>
2023-10-28 16:36:20 -0300

    AB#30: hotfix: ajuste necessário para especidade do projeto

--- web/start_with_docker.sh
+++ web/start_with_docker.sh
@@ -2,4 +2,4 @@
 docker build -t frontend:latest .
 
 # 2 Run Application
-docker run --name Frontend -d -p 0.0.0.0:4200:4200 -v `pwd`/src:/app/src frontend:latest
+docker run --name Frontend -d -p 0.0.0.0:4206:4206 -v `pwd`/src:/app/src frontend:latest
