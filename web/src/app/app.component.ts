cc1970e166cf5c8d0c17583281353958cbc06d3d
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2023-11-01 12:00:31 -0300

    AB#87 feat: create routing

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
   standalone: true,
-  imports: [CommonModule, BrowserModule, RouterModule]
+  imports: [CommonModule, RouterModule]
 })
 export class AppComponent {
   title = 'web';
