3abfde31a51c48000393fa84152b99ee19ded763
Eduardo Oliveira <eduardo123fernandes2013@hotmail.com>
2023-11-01 21:55:36 -0300

    wip (home): estrutura básica da home AB#49

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -2,15 +2,21 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
     path: 'auth',
     loadChildren: async () => (await import('./pages/sign-in/auth/auth.module')).AuthModule
   },
   {
     // home
-    path: ''
-  },
-  {
-    // sistema
-    path: ''
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent)
   }
+  // {
+  //   // sistema
+  //   path: ''
+  // }
 ];
