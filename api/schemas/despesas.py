0553cc2e7f587fee17ad69eca4af8ff0473c216f
Davi Antonaji <davi@antonaji.com.br>
2023-11-09 20:26:40 -0300

    Removing schema

--- api/schemas/despesas.py
+++ api/schemas/despesas.py
@@ -1,13 +1,1 @@
-from marshmallow import Schema, fields
 
-
-############
-# Requests #
-############
-
-class DespesaRegisterSchema(Schema):
-    user_id = fields.Str(required=True)
-    descricao = fields.Str(required=True)
-    valor = fields.Float(required=True)
-    data = fields.Str(required=True)
-    categoria = fields.Str(required=True)
