af68f3be8c98ab05d312904974ab2d4e644e138a
Eduardo Oliveira <eduardo123fernandes2013@hotmail.com>
2023-11-15 16:49:12 -0300

    feat (listar despesas): listando despesas AB#61 refeito commit, esqueci de colcoar AB#

---
+++ web/src/app/core/models/despesa.ts
@@ -0,0 +1,7 @@
+export class Despesa {
+  category: string;
+  date: Date;
+  description: string;
+  user_id: string;
+  value: number;
+}
--- web/src/app/pages/despesas/despesas.component.ts
+++ web/src/app/pages/despesas/despesas.component.ts
@@ -1,23 +1,81 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { NgbModal, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { Despesa } from 'src/app/core/models/despesa';
 import { CreateDespesaComponent } from './create-despesa/create-despesa.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, NgbModalModule],
+  imports: [CommonModule, NgbModalModule, DatePipe],
   templateUrl: './despesas.component.html',
   styleUrls: ['./despesas.component.scss']
 })
 export class DespesasComponent implements OnInit {
+  despesas: Despesa[] = [
+    {
+      category: 'Lazer',
+      date: new Date(),
+      description: 'Ingresso',
+      user_id: 'o|zkR|cKKoeSYJ3IJWXrWA==',
+      value: 299
+    },
+    {
+      category: 'Lazer',
+      date: new Date(),
+      description: 'Ingresso',
+      user_id: 'o|zkR|cKKoeSYJ3IJWXrWA==',
+      value: 299
+    },
+    {
+      category: 'Lazer',
+      date: new Date(),
+      description: 'Ingresso',
+      user_id: 'o|zkR|cKKoeSYJ3IJWXrWA==',
+      value: 299
+    },
+    {
+      category: 'Lazer',
+      date: new Date(),
+      description: 'Ingresso',
+      user_id: 'o|zkR|cKKoeSYJ3IJWXrWA==',
+      value: 299
+    },
+    {
+      category: 'Lazer',
+      date: new Date(),
+      description: 'Ingresso',
+      user_id: 'o|zkR|cKKoeSYJ3IJWXrWA==',
+      value: 299
+    },
+    {
+      category: 'Lazer',
+      date: new Date(),
+      description: 'Ingresso',
+      user_id: 'o|zkR|cKKoeSYJ3IJWXrWA==',
+      value: 299
+    }
+  ];
+
   constructor(private ngbModal: NgbModal) {}
 
   ngOnInit(): void {}
 
-  openMoalDespesa(): void {
-    this.ngbModal.open(CreateDespesaComponent, {
+  editDespesa(despesa: Despesa): void {
+    this.openMoalDespesa(despesa);
+  }
+
+  async openMoalDespesa(despesa: Despesa | null = null): Promise<void> {
+    const modalRef = this.ngbModal.open(CreateDespesaComponent, {
       size: 'lg',
       centered: true
     });
+
+    if (despesa) {
+      modalRef.componentInstance.despesa = despesa;
+    }
+
+    const result = await modalRef.result;
+
+    console.log(result);
   }
 }
