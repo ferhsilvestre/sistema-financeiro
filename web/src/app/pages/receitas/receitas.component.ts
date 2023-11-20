84c5ed57cf7fc01f08adbe2d8b12006ee8a37ae1
Eduardo Oliveira <eduardo123fernandes2013@hotmail.com>
2023-11-20 20:14:32 -0300

    feat (receitas): modal cadastro AB#67

--- web/src/app/pages/receitas/receitas.component.ts
+++ web/src/app/pages/receitas/receitas.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Receita } from 'src/app/core/models/receita';
+import { CreateReceitaComponent } from './create-receita/create-receita.component';
 
 @Component({
   standalone: true,
@@ -7,6 +10,29 @@ import { CommonModule } from '@angular/common';
   templateUrl: './receitas.component.html',
   styleUrls: ['./receitas.component.scss']
 })
-export class ReceitasComponent {
+export class ReceitasComponent implements OnInit {
+  constructor(private ngbModal: NgbModal) {}
 
+  ngOnInit(): void {
+    this.openMoalReceita();
+  }
+
+  editDespesa(receita: Receita): void {
+    this.openMoalReceita(receita);
+  }
+
+  async openMoalReceita(receita: Receita | null = null): Promise<void> {
+    const modalRef = this.ngbModal.open(CreateReceitaComponent, {
+      size: 'lg',
+      centered: true
+    });
+
+    if (receita) {
+      modalRef.componentInstance.receita = receita;
+    }
+
+    const result = await modalRef.result;
+
+    console.log(result);
+  }
 }
